3cefb888b9339914552fe3dd68e3da1877bc9555
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-20 04:12:18 +0000

    leetie: Add 220. Contains Duplicate III [Hard] (c)

--- solutions/Hard/0220-contains-duplicate-iii/solution.c
+++ solutions/Hard/0220-contains-duplicate-iii/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Sliding Window, Sorting, Bucket Sort, Ordered Set
 // Link     : https://leetcode.com/problems/contains-duplicate-iii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8472000 (beats 0%)
+// Runtime  : 117 ms (beats 37%)
+// Memory   : 52808000 (beats 50%)
 // Language : c
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
