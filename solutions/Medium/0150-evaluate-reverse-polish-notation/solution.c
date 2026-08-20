dbf78e59afcefc7eea437f103efe10070b8037e2
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-20 03:55:47 +0000

    leetie: Add 150. Evaluate Reverse Polish Notation [Medium] (c)

--- solutions/Medium/0150-evaluate-reverse-polish-notation/solution.c
+++ solutions/Medium/0150-evaluate-reverse-polish-notation/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, Stack
 // Link     : https://leetcode.com/problems/evaluate-reverse-polish-notation/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8648000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10312000 (beats 61%)
 // Language : c
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
