0b142af45c943628bb54774146aaafbbc5c3cdd1
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-20 04:18:27 +0000

    leetie: Add 274. H-Index [Medium] (c)

--- solutions/Medium/0274-h-index/solution.c
+++ solutions/Medium/0274-h-index/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Sorting, Counting Sort
 // Link     : https://leetcode.com/problems/h-index/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8516000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9080000 (beats 62%)
 // Language : c
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
