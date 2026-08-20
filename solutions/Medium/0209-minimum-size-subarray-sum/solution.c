330c3126c4b2006a1275cbcfe54012f763995f58
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-20 04:04:51 +0000

    leetie: Add 209. Minimum Size Subarray Sum [Medium] (c)

--- solutions/Medium/0209-minimum-size-subarray-sum/solution.c
+++ solutions/Medium/0209-minimum-size-subarray-sum/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search, Sliding Window, Prefix Sum
 // Link     : https://leetcode.com/problems/minimum-size-subarray-sum/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8436000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 13868000 (beats 92%)
 // Language : c
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
