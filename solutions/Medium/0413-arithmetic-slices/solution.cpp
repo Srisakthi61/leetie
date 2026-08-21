208ae57dcfaa5f7ae14219e0e8ade0b2cfcc6c6f
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 10:55:46 +0000

    leetie: Add 413. Arithmetic Slices [Medium] (cpp)

--- solutions/Medium/0413-arithmetic-slices/solution.cpp
+++ solutions/Medium/0413-arithmetic-slices/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Sliding Window
 // Link     : https://leetcode.com/problems/arithmetic-slices/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8148000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10624000 (beats 99%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
