c72dca0aad4df7a043fd4cf136dc866ccfb0a97f
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 11:14:45 +0000

    leetie: Add 472. Concatenated Words [Hard] (cpp)

--- solutions/Hard/0472-concatenated-words/solution.cpp
+++ solutions/Hard/0472-concatenated-words/solution.cpp
@@ -4,7 +4,7 @@
 // Tags     : Array, String, Dynamic Programming, Depth-First Search, Trie, Sorting
 // Link     : https://leetcode.com/problems/concatenated-words/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 8528000 (beats 0%)
+// Memory   : 8644000 (beats 0%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
@@ -15,8 +15,10 @@ public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> word_set(words.begin(), words.end());
         vector<string> result;
+        unordered_map<string, bool> memo;
         
         auto canForm = [&](string& word, auto& self) -> bool {
+            if (memo.count(word)) return memo[word];
             int len = word.length();
             for (int i = 1; i < len; ++i) {
                 string prefix = word.substr(0, i);
@@ -24,11 +26,11 @@ public:
                 
                 if (word_set.count(prefix)) {
                     if (word_set.count(suffix) || self(suffix, self)) {
-                        return true;
+                        return memo[word] = true;
                     }
                 }
             }
-            return false;
+            return memo[word] = false;
         };
         
         for (string& word : words) {
