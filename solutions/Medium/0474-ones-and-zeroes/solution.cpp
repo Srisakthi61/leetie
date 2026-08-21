b3d3c040c4a42e65cab25e6c7dc4efccbcbb846a
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 11:17:22 +0000

    leetie: Add 474. Ones and Zeroes [Medium] (cpp)

--- solutions/Medium/0474-ones-and-zeroes/solution.cpp
+++ solutions/Medium/0474-ones-and-zeroes/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, String, Dynamic Programming, Knapsack Problem, 0-1 Knapsack
 // Link     : https://leetcode.com/problems/ones-and-zeroes/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8424000 (beats 0%)
+// Runtime  : 47 ms (beats 90%)
+// Memory   : 13620000 (beats 94%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
