b322dea454229b2d775caba8219ce21cb8a52573
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 10:51:28 +0000

    leetie: Add 406. Queue Reconstruction by Height [Medium] (cpp)

--- solutions/Medium/0406-queue-reconstruction-by-height/solution.cpp
+++ solutions/Medium/0406-queue-reconstruction-by-height/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Indexed Tree, Segment Tree, Sorting
 // Link     : https://leetcode.com/problems/queue-reconstruction-by-height/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8440000 (beats 0%)
+// Runtime  : 18 ms (beats 66%)
+// Memory   : 16104000 (beats 43%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
