176436e39348072a8a3265ef718121941fd4b2f1
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 03:57:47 +0000

    leetie: Add 1482. Minimum Number of Days to Make m Bouquets [Medium] (java)

--- solutions/Medium/1482-minimum-number-of-days-to-make-m-bouquets/solution.java
+++ solutions/Medium/1482-minimum-number-of-days-to-make-m-bouquets/solution.java
@@ -3,38 +3,47 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
-// Runtime  : 14 ms (beats 99%)
-// Memory   : 82448000 (beats 84%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42688000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
 // ──────────────────────────────────────────────────
 
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int low=1;
-        int max=0;
-        if(bloomDay.length <(long)m*k){
-            return -1;
-        }
-        for(int c:bloomDay){
-            max=Math.max(max,c);
-        }
-        int high=max;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int flower=0;
-            int bq=0;
+    boolean possible(int[] bloomDay, int m, int k, int day){
+        int flower=0;
+        int no_of_bq=0;
             for(int i=0; i<bloomDay.length; i++){
-                if(bloomDay[i]<=mid){
+                if(bloomDay[i]<=day){
                     flower++;
                 }else{
-                    bq+=flower/k;
+                    no_of_bq+=flower/k;
                     flower=0;
                 }
             }
-            bq+=flower/k;
-            if(bq>=m){
+            no_of_bq+=flower/k;
+            if(no_of_bq>=m){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int c:bloomDay){
+            max=Math.max(max,c);
+            min=Math.min(min,c);
+        }
+        int low=min;
+        int high=max;
+        if(bloomDay.length <(long) m*k){
+            return -1;
+        }
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(bloomDay, m, k, mid)){
                 high=mid-1;
             }else{
                 low=mid+1;
