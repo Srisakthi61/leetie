b1c916dfc3bf5f1ad7c3bc1520b0ac9ca6d54a77
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-21 11:17:50 +0000

    leetie: Add 475. Heaters [Medium] (cpp)

--- solutions/Medium/0475-heaters/solution.cpp
+++ solutions/Medium/0475-heaters/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Two Pointers, Binary Search, Sorting
 // Link     : https://leetcode.com/problems/heaters/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8104000 (beats 0%)
+// Runtime  : 20 ms (beats 36%)
+// Memory   : 29148000 (beats 36%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
