8196154e6a2b3704dee00657ea20aeadeba0e37e
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-22 17:54:30 +0000

    leetie: Add 434. Number of Segments in a String [Easy] (cpp)

--- solutions/Easy/0434-number-of-segments-in-a-string/solution.cpp
+++ solutions/Easy/0434-number-of-segments-in-a-string/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : String
 // Link     : https://leetcode.com/problems/number-of-segments-in-a-string/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7752000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8344000 (beats 21%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
