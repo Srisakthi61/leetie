253be2e264447209c9560d994c0c37043a7988de
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-22 18:12:54 +0000

    leetie: Add 498. Diagonal Traverse [Medium] (cpp)

--- solutions/Medium/0498-diagonal-traverse/solution.cpp
+++ solutions/Medium/0498-diagonal-traverse/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Matrix, Simulation
 // Link     : https://leetcode.com/problems/diagonal-traverse/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8212000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 22708000 (beats 70%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
