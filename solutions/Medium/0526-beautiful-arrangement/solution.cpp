cd237a587919fd2332da8a9344cc63539b7861d5
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-23 16:25:34 +0000

    leetie: Add 526. Beautiful Arrangement [Medium] (cpp)

--- solutions/Medium/0526-beautiful-arrangement/solution.cpp
+++ solutions/Medium/0526-beautiful-arrangement/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Backtracking, Bit Manipulation, Bitmask
 // Link     : https://leetcode.com/problems/beautiful-arrangement/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7964000 (beats 0%)
+// Runtime  : 46 ms (beats 48%)
+// Memory   : 7908000 (beats 61%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
