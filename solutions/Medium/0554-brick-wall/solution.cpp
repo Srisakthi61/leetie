23946c0b61b0929fa4c2400da4b613a484317d7a
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-23 16:32:05 +0000

    leetie: Add 554. Brick Wall [Medium] (cpp)

--- solutions/Medium/0554-brick-wall/solution.cpp
+++ solutions/Medium/0554-brick-wall/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table
 // Link     : https://leetcode.com/problems/brick-wall/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8296000 (beats 0%)
+// Runtime  : 4 ms (beats 89%)
+// Memory   : 26528000 (beats 56%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
