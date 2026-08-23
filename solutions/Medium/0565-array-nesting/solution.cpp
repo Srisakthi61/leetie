78c5451fa8a285fc8a613f22db276ece89ecd811
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-23 16:33:50 +0000

    leetie: Add 565. Array Nesting [Medium] (cpp)

--- solutions/Medium/0565-array-nesting/solution.cpp
+++ solutions/Medium/0565-array-nesting/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Depth-First Search
 // Link     : https://leetcode.com/problems/array-nesting/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8260000 (beats 0%)
+// Runtime  : 6 ms (beats 55%)
+// Memory   : 97048000 (beats 40%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
