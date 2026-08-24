a13bc1d08c78f6a1e361dfc931400cdacc09afbf
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 03:19:16 +0000

    leetie: Add 636. Exclusive Time of Functions [Medium] (cpp)

---
+++ solutions/Medium/0636-exclusive-time-of-functions/solution.cpp
@@ -0,0 +1,43 @@
+// ──────────────────────────────────────────────────
+// Problem  : 636. Exclusive Time of Functions
+// Difficulty: Medium
+// Tags     : Array, Stack
+// Link     : https://leetcode.com/problems/exclusive-time-of-functions/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8488000 (beats 0%)
+// Language : cpp
+// Copyright: (c) 2026 Srisakthi61. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> res(n, 0);
+        stack<int> st;
+        int prevTime = 0;
+        
+        for (const string& log : logs) {
+            int firstColon = log.find(':');
+            int secondColon = log.rfind(':');
+            
+            int funcId = stoi(log.substr(0, firstColon));
+            string type = log.substr(firstColon + 1, secondColon - firstColon - 1);
+            int time = stoi(log.substr(secondColon + 1));
+            
+            if (type == "start") {
+                if (!st.empty()) {
+                    res[st.top()] += time - prevTime;
+                }
+                st.push(funcId);
+                prevTime = time;
+            } else {
+                res[st.top()] += time - prevTime + 1;
+                st.pop();
+                prevTime = time + 1;
+            }
+        }
+        
+        return res;
+    }
+};
