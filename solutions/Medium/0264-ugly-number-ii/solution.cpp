e1b03f690e36dce74cb15a94809b85a65a218f9f
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:41:56 +0000

    leetie: Add 264. Ugly Number II [Medium] (cpp)

--- solutions/Medium/0264-ugly-number-ii/solution.cpp
+++ solutions/Medium/0264-ugly-number-ii/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Hash Table, Math, Dynamic Programming, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/ugly-number-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7708000 (beats 0%)
+// Runtime  : 3 ms (beats 78%)
+// Memory   : 11292000 (beats 83%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
