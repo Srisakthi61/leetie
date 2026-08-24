31b5ae961a65a29106830a113690e8f3111e83e5
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:41:15 +0000

    leetie: Add 263. Ugly Number [Easy] (cpp)

--- solutions/Easy/0263-ugly-number/solution.cpp
+++ solutions/Easy/0263-ugly-number/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math
 // Link     : https://leetcode.com/problems/ugly-number/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7764000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7660000 (beats 98%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
