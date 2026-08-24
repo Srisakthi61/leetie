a5f569a01cbe5dbb6cab52da5de2341a40c127be
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:42:53 +0000

    leetie: Add 292. Nim Game [Easy] (cpp)

--- solutions/Easy/0292-nim-game/solution.cpp
+++ solutions/Easy/0292-nim-game/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Brainteaser, Minimax, Game Theory, Nim Game, Impartial Game
 // Link     : https://leetcode.com/problems/nim-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7716000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7900000 (beats 17%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
