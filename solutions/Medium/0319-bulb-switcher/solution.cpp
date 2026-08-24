a9d6c0d31177a3eae017ff4528a4e7ec672e7942
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:43:22 +0000

    leetie: Add 319. Bulb Switcher [Medium] (cpp)

--- solutions/Medium/0319-bulb-switcher/solution.cpp
+++ solutions/Medium/0319-bulb-switcher/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Brainteaser
 // Link     : https://leetcode.com/problems/bulb-switcher/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7892000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7724000 (beats 69%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
