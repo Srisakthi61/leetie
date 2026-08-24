8301b112e82204d5f644d6952b44f318a6c621d0
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:39:04 +0000

    leetie: Add 231. Power of Two [Easy] (cpp)

--- solutions/Easy/0231-power-of-two/solution.cpp
+++ solutions/Easy/0231-power-of-two/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Bit Manipulation, Recursion
 // Link     : https://leetcode.com/problems/power-of-two/
-// Runtime  : 0 ms (beats 100%)
-// Memory   : 7720000 (beats 82%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 7796000 (beats 0%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
