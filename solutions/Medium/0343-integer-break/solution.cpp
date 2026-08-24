3e8ef056635a424dda0780288f24e16ecb0b4339
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:45:03 +0000

    leetie: Add 343. Integer Break [Medium] (cpp)

--- solutions/Medium/0343-integer-break/solution.cpp
+++ solutions/Medium/0343-integer-break/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Dynamic Programming
 // Link     : https://leetcode.com/problems/integer-break/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7680000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7952000 (beats 69%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
