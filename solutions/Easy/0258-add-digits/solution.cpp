1608dd28ad694ad109bf498963f73cf5a7d09f81
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:40:46 +0000

    leetie: Add 258. Add Digits [Easy] (cpp)

--- solutions/Easy/0258-add-digits/solution.cpp
+++ solutions/Easy/0258-add-digits/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Simulation, Number Theory
 // Link     : https://leetcode.com/problems/add-digits/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7988000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8512000 (beats 80%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
