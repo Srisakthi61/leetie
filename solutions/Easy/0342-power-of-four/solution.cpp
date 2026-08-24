ad104c8f9803ddc2496963c7fac6bc51b00337a4
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-24 17:44:28 +0000

    leetie: Add 342. Power of Four [Easy] (cpp)

--- solutions/Easy/0342-power-of-four/solution.cpp
+++ solutions/Easy/0342-power-of-four/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Bit Manipulation, Recursion
 // Link     : https://leetcode.com/problems/power-of-four/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7768000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7788000 (beats 81%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
