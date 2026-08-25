892a8a18e9875d40572aa66f1d46319043042466
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 04:51:14 +0000

    leetie: Add 492. Construct the Rectangle [Easy] (cpp)

--- solutions/Easy/0492-construct-the-rectangle/solution.cpp
+++ solutions/Easy/0492-construct-the-rectangle/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math
 // Link     : https://leetcode.com/problems/construct-the-rectangle/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7932000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7880000 (beats 100%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
