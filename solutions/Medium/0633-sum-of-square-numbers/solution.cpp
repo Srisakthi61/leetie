1b7af77a3dfb40dc088bed681ade7352ccfe0860
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 05:00:56 +0000

    leetie: Add 633. Sum of Square Numbers [Medium] (cpp)

--- solutions/Medium/0633-sum-of-square-numbers/solution.cpp
+++ solutions/Medium/0633-sum-of-square-numbers/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Two Pointers, Binary Search
 // Link     : https://leetcode.com/problems/sum-of-square-numbers/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7876000 (beats 0%)
+// Runtime  : 3 ms (beats 30%)
+// Memory   : 7816000 (beats 23%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
