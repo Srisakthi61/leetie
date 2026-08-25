3e3d8c1529520797b16a4af29b8d00886d00cd4e
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 04:50:48 +0000

    leetie: Add 483. Smallest Good Base [Hard] (cpp)

--- solutions/Hard/0483-smallest-good-base/solution.cpp
+++ solutions/Hard/0483-smallest-good-base/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Math, Binary Search
 // Link     : https://leetcode.com/problems/smallest-good-base/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7972000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8056000 (beats 74%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
