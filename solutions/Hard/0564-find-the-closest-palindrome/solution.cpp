6d3dac4b9d2a48890bc04ee7df5b109cc99380b0
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 04:57:53 +0000

    leetie: Add 564. Find the Closest Palindrome [Hard] (cpp)

--- solutions/Hard/0564-find-the-closest-palindrome/solution.cpp
+++ solutions/Hard/0564-find-the-closest-palindrome/solution.cpp
@@ -4,7 +4,7 @@
 // Tags     : Math, String
 // Link     : https://leetcode.com/problems/find-the-closest-palindrome/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 8064000 (beats 0%)
+// Memory   : 8140000 (beats 0%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
@@ -17,8 +17,8 @@ public:
         long long num = stoll(n);
         vector<long long> candidates;
         
-        candidates.push_back(stoll("1" + string(len - 1, '0') + "1") - 2);
-        candidates.push_back(stoll("1" + string(len, '0')) + 1);
+        candidates.push_back(pow(10, len - 1) - 1);
+        candidates.push_back(pow(10, len) + 1);
         
         long long mid = (len + 1) / 2;
         long long prefix = stoll(n.substr(0, mid));
