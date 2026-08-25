1906200fbd0bede1d2d73c1a7511032e663a6222
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 04:52:55 +0000

    leetie: Add 509. Fibonacci Number [Easy] (cpp)

--- solutions/Easy/0509-fibonacci-number/solution.cpp
+++ solutions/Easy/0509-fibonacci-number/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Dynamic Programming, Recursion, Memoization
 // Link     : https://leetcode.com/problems/fibonacci-number/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7572000 (beats 0%)
+// Runtime  : 3 ms (beats 65%)
+// Memory   : 7900000 (beats 39%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
