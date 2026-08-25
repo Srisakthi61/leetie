42929e909fb2cd2674ea923b475e67516feec8e5
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-25 04:50:31 +0000

    leetie: Add 483. Smallest Good Base [Hard] (cpp)

--- solutions/Hard/0483-smallest-good-base/solution.cpp
+++ solutions/Hard/0483-smallest-good-base/solution.cpp
@@ -4,7 +4,7 @@
 // Tags     : Math, Binary Search
 // Link     : https://leetcode.com/problems/smallest-good-base/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 8104000 (beats 0%)
+// Memory   : 7972000 (beats 0%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
@@ -15,7 +15,7 @@ public:
     string smallestGoodBase(string n) {
         unsigned long long num = stoull(n);
         for (int k = 60; k >= 2; --k) {
-            unsigned long long l = 1, r = pow(num, 1.0 / (k - 1)) + 1;
+            unsigned long long l = 2, r = pow(num, 1.0 / (k - 1)) + 1;
             while (l <= r) {
                 unsigned long long m = l + (r - l) / 2;
                 unsigned long long sum = 0, cur = 1;
