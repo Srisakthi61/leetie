76a2e9bb7a4b7f0e75be6f93556026c6cb854316
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:38:17 +0000

    leetie: Add 869. Reordered Power of 2 [Medium] (cpp)

---
+++ solutions/Medium/0869-reordered-power-of-2/solution.cpp
@@ -0,0 +1,27 @@
+// ──────────────────────────────────────────────────
+// Problem  : 869. Reordered Power of 2
+// Difficulty: Medium
+// Tags     : Hash Table, Math, Sorting, Counting, Enumeration
+// Link     : https://leetcode.com/problems/reordered-power-of-2/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 7940000 (beats 0%)
+// Language : cpp
+// Copyright: (c) 2026 Srisakthi61. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        for (int i = 0; i < 31; ++i) {
+            string p = to_string(1 << i);
+            sort(p.begin(), p.end());
+            if (s == p) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
