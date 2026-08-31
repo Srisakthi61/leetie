6cf805235134605e2ddca2345e79023af0e99af6
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:26:37 +0000

    leetie: Add 810. Chalkboard XOR Game [Hard] (cpp)

--- solutions/Hard/0810-chalkboard-xor-game/solution.cpp
+++ solutions/Hard/0810-chalkboard-xor-game/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Math, Bit Manipulation, Brainteaser, Game Theory, Zero-Sum Game, Impartial Game
 // Link     : https://leetcode.com/problems/chalkboard-xor-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8008000 (beats 0%)
+// Runtime  : 1 ms (beats 6%)
+// Memory   : 16584000 (beats 56%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
