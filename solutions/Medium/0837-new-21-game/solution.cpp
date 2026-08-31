4f5ac813a0f61d6e94a25e01e0f2710992b32c8a
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:31:50 +0000

    leetie: Add 837. New 21 Game [Medium] (cpp)

--- solutions/Medium/0837-new-21-game/solution.cpp
+++ solutions/Medium/0837-new-21-game/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Dynamic Programming, Sliding Window, Probability and Statistics
 // Link     : https://leetcode.com/problems/new-21-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8124000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 12500000 (beats 76%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
