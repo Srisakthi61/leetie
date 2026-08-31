33c5353f79171c30b061643ba28a87b77313dd4e
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:43:45 +0000

    leetie: Add 892. Surface Area of 3D Shapes [Easy] (cpp)

--- solutions/Easy/0892-surface-area-of-3d-shapes/solution.cpp
+++ solutions/Easy/0892-surface-area-of-3d-shapes/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Math, Geometry, Matrix
 // Link     : https://leetcode.com/problems/surface-area-of-3d-shapes/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8336000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 12984000 (beats 40%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
