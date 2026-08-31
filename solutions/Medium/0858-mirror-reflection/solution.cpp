b17f78106e4e9fe2fed09fc5a4b3d72ce2a2957a
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:35:41 +0000

    leetie: Add 858. Mirror Reflection [Medium] (cpp)

--- solutions/Medium/0858-mirror-reflection/solution.cpp
+++ solutions/Medium/0858-mirror-reflection/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Geometry, Number Theory, Least Common Multiple, Greatest Common Divisor
 // Link     : https://leetcode.com/problems/mirror-reflection/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7664000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7788000 (beats 81%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
