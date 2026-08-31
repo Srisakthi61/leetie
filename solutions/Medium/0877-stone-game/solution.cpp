c9ff95ef1b74a4984cfa4b9ce7cd67883e14bb59
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-08-31 16:40:14 +0000

    leetie: Add 877. Stone Game [Medium] (cpp)

--- solutions/Medium/0877-stone-game/solution.cpp
+++ solutions/Medium/0877-stone-game/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, Dynamic Programming, Minimax, Game Theory, Zero-Sum Game
 // Link     : https://leetcode.com/problems/stone-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7988000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10508000 (beats 42%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
