da3f5181ee3afedaeeecc9c1b7ff3c92d7362166
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 17:11:33 +0000

    leetie: Add 996. Number of Squareful Arrays [Hard] (cpp)

--- solutions/Hard/0996-number-of-squareful-arrays/solution.cpp
+++ solutions/Hard/0996-number-of-squareful-arrays/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Hash Table, Math, Dynamic Programming, Backtracking, Bit Manipulation, Bitmask
 // Link     : https://leetcode.com/problems/number-of-squareful-arrays/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8188000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10056000 (beats 77%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
