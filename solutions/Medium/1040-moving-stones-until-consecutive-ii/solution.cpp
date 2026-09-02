034cde9276c7dd67fd7fd5e1278f3feeded26221
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 17:18:31 +0000

    leetie: Add 1040. Moving Stones Until Consecutive II [Medium] (cpp)

--- solutions/Medium/1040-moving-stones-until-consecutive-ii/solution.cpp
+++ solutions/Medium/1040-moving-stones-until-consecutive-ii/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, Sliding Window, Sorting
 // Link     : https://leetcode.com/problems/moving-stones-until-consecutive-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8180000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 17112000 (beats 4%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
