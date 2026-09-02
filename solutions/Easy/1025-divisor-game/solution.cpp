f495824d8c77caa1a5a08ae1135f79c5bf0e5cb4
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 17:14:51 +0000

    leetie: Add 1025. Divisor Game [Easy] (cpp)

--- solutions/Easy/1025-divisor-game/solution.cpp
+++ solutions/Easy/1025-divisor-game/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Dynamic Programming, Brainteaser, Game Theory, Impartial Game
 // Link     : https://leetcode.com/problems/divisor-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7760000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7708000 (beats 58%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
