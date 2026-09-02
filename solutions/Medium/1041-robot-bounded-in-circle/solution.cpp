8aaf7bb6b508a408e9fffe2106bf0736a695583a
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 17:19:16 +0000

    leetie: Add 1041. Robot Bounded In Circle [Medium] (cpp)

--- solutions/Medium/1041-robot-bounded-in-circle/solution.cpp
+++ solutions/Medium/1041-robot-bounded-in-circle/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, String, Simulation
 // Link     : https://leetcode.com/problems/robot-bounded-in-circle/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7748000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8216000 (beats 47%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
