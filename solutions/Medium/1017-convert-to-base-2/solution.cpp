1952a36916bb83bdda09cb863823c35d18f5546e
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 17:14:16 +0000

    leetie: Add 1017. Convert to Base -2 [Medium] (cpp)

--- solutions/Medium/1017-convert-to-base-2/solution.cpp
+++ solutions/Medium/1017-convert-to-base-2/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math
 // Link     : https://leetcode.com/problems/convert-to-base-2/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7752000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9120000 (beats 9%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
