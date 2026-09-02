bd70ee3815c6b0820aeb108cad41254afa6d4ebd
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 16:58:55 +0000

    leetie: Add 920. Number of Music Playlists [Hard] (cpp)

--- solutions/Hard/0920-number-of-music-playlists/solution.cpp
+++ solutions/Hard/0920-number-of-music-playlists/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Math, Dynamic Programming, Combinatorics
 // Link     : https://leetcode.com/problems/number-of-music-playlists/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7780000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 7836000 (beats 95%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
