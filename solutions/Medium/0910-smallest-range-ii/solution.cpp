982dcbcd70c5e5f1b588a403688ab5cac958a405
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-02 16:56:20 +0000

    leetie: Add 910. Smallest Range II [Medium] (cpp)

--- solutions/Medium/0910-smallest-range-ii/solution.cpp
+++ solutions/Medium/0910-smallest-range-ii/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, Greedy, Sorting
 // Link     : https://leetcode.com/problems/smallest-range-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8384000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 19284000 (beats 76%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
