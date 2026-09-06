dcb52e9421e216a79d1b7e479a948b280b145c79
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:34:01 +0000

    leetie: Add 923. 3Sum With Multiplicity [Medium] (cpp)

--- solutions/Medium/0923-3sum-with-multiplicity/solution.cpp
+++ solutions/Medium/0923-3sum-with-multiplicity/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, Two Pointers, Sorting, Counting
 // Link     : https://leetcode.com/problems/3sum-with-multiplicity/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8300000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 14024000 (beats 90%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
