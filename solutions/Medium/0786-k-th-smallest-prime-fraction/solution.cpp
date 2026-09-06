ec97d61620648f3a04cfa3bfb4f70fb9f3b1f390
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:17:28 +0000

    leetie: Add 786. K-th Smallest Prime Fraction [Medium] (cpp)

--- solutions/Medium/0786-k-th-smallest-prime-fraction/solution.cpp
+++ solutions/Medium/0786-k-th-smallest-prime-fraction/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Two Pointers, Binary Search, Sorting, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/k-th-smallest-prime-fraction/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8332000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11800000 (beats 97%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
