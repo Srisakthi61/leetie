8e88b084bbfa9fdc193889749382a70a7da78e86
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:11:34 +0000

    leetie: Add 721. Accounts Merge [Medium] (cpp)

--- solutions/Medium/0721-accounts-merge/solution.cpp
+++ solutions/Medium/0721-accounts-merge/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, String, Depth-First Search, Breadth-First Search, Union-Find, Sorting
 // Link     : https://leetcode.com/problems/accounts-merge/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8988000 (beats 0%)
+// Runtime  : 20 ms (beats 97%)
+// Memory   : 38152000 (beats 92%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
