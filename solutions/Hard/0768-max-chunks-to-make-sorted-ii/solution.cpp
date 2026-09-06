56c6f34582acf779e07b3841ed56dfe99c714a39
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:15:58 +0000

    leetie: Add 768. Max Chunks To Make Sorted II [Hard] (cpp)

---
+++ solutions/Hard/0768-max-chunks-to-make-sorted-ii/solution.cpp
@@ -0,0 +1,38 @@
+// ──────────────────────────────────────────────────
+// Problem  : 768. Max Chunks To Make Sorted II
+// Difficulty: Hard
+// Tags     : Array, Stack, Greedy, Sorting, Monotonic Stack
+// Link     : https://leetcode.com/problems/max-chunks-to-make-sorted-ii/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8036000 (beats 0%)
+// Language : cpp
+// Copyright: (c) 2026 Srisakthi61. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> maxOfLeft(n);
+        vector<int> minOfRight(n + 1, INT_MAX);
+
+        maxOfLeft[0] = arr[0];
+        for (int i = 1; i < n; ++i) {
+            maxOfLeft[i] = max(maxOfLeft[i - 1], arr[i]);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            minOfRight[i] = min(minOfRight[i + 1], arr[i]);
+        }
+
+        int chunks = 0;
+        for (int i = 0; i < n; ++i) {
+            if (maxOfLeft[i] <= minOfRight[i + 1]) {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+};
