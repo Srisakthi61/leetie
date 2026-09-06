10f0df362cf84f05c51489688de5066dc50cc60c
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:37:54 +0000

    leetie: Add 969. Pancake Sorting [Medium] (cpp)

--- solutions/Medium/0969-pancake-sorting/solution.cpp
+++ solutions/Medium/0969-pancake-sorting/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Two Pointers, Greedy, Sorting
 // Link     : https://leetcode.com/problems/pancake-sorting/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8292000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 14212000 (beats 16%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
