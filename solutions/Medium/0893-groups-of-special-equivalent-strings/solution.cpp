203677ff2657e601b10da473b9355232c6bfb3a6
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:31:49 +0000

    leetie: Add 893. Groups of Special-Equivalent Strings [Medium] (cpp)

---
+++ solutions/Medium/0893-groups-of-special-equivalent-strings/solution.cpp
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Problem  : 893. Groups of Special-Equivalent Strings
+// Difficulty: Medium
+// Tags     : Array, Hash Table, String, Sorting
+// Link     : https://leetcode.com/problems/groups-of-special-equivalent-strings/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8416000 (beats 0%)
+// Language : cpp
+// Copyright: (c) 2026 Srisakthi61. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& words) {
+        unordered_set<string> seen;
+        for (string& word : words) {
+            string odd = "", even = "";
+            for (int i = 0; i < word.length(); ++i) {
+                if (i % 2 == 0) {
+                    even += word[i];
+                } else {
+                    odd += word[i];
+                }
+            }
+            sort(even.begin(), even.end());
+            sort(odd.begin(), odd.end());
+            seen.insert(even + odd);
+        }
+        return seen.size();
+    }
+};
