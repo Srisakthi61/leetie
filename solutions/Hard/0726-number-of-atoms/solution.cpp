2ec980df9588ed1e7631da0661a82ccd33d86708
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:12:12 +0000

    leetie: Add 726. Number of Atoms [Hard] (cpp)

--- solutions/Hard/0726-number-of-atoms/solution.cpp
+++ solutions/Hard/0726-number-of-atoms/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Hash Table, String, Stack, Sorting
 // Link     : https://leetcode.com/problems/number-of-atoms/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 7880000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9648000 (beats 92%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
