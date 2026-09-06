cacd34d19385085be3df3523d258b55e95e68509
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-06 17:33:32 +0000

    leetie: Add 922. Sort Array By Parity II [Easy] (cpp)

--- solutions/Easy/0922-sort-array-by-parity-ii/solution.cpp
+++ solutions/Easy/0922-sort-array-by-parity-ii/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Two Pointers, Sorting
 // Link     : https://leetcode.com/problems/sort-array-by-parity-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8284000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 25712000 (beats 96%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
