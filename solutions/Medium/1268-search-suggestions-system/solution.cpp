cd7cf3d5cb3ef797d37c52e838462f1d0f45a261
Srisakthi61 <srisakthi61@users.noreply.github.com>
2026-09-10 04:26:38 +0000

    leetie: Add 1268. Search Suggestions System [Medium] (cpp)

--- solutions/Medium/1268-search-suggestions-system/solution.cpp
+++ solutions/Medium/1268-search-suggestions-system/solution.cpp
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, String, Binary Search, Trie, Sorting, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/search-suggestions-system/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8928000 (beats 0%)
+// Runtime  : 14 ms (beats 78%)
+// Memory   : 35788000 (beats 69%)
 // Language : cpp
 // Copyright: (c) 2026 Srisakthi61. All rights reserved.
 // Synced by: leetie
